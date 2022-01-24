a79172e06782e898030b67029cf43e4e29e791fd
RafaelButler <rafaelbutler80@gmail.com>
2022-01-24 09:45:58 -0300

    setup laravel project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [AboutController::class, 'show']);
+Route::get('/', function () {
+    return view('welcome');
+});
